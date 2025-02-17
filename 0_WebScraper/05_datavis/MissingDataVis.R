dfe0a8d2963dc4c86013ffc76f2d00cb1d7f4a6e
nickc0007 <nicholasclausen846@gmail.com>
2025-02-17 15:58:14 -0700

    Data Vis

--- 0_WebScraper/05_datavis/MissingDataVis.R
+++ 0_WebScraper/05_datavis/MissingDataVis.R
@@ -11,16 +11,26 @@ for (file in file_list) {
   print(paste("Processing file:", file))
 }
 
-
-data_vars <- ls(pattern = "^data_")
-for (var in data_vars) {
+tempdata_vars <- ls(pattern = "^data_")
+for (var in tempdata_vars) {
   print(paste("Processing missing value plot for:", var))
   
-  p <- vis_miss(get(var)) + ggtitle(paste("Missing Values in", var))
+  tempdata_df <- get(var)
   
-  plot_filename <- file.path("05_datavis", paste0(var, "_missing_plot.jpeg"))
+  if (nrow(tempdata_df) == 0) {
+    print(paste("Skipping", var, "because the data frame is empty"))
+    next  # Skip this iteration if the dataframe is empty
+  }
   
-  ggsave(filename = plot_filename, plot = p, width = 8, height = 6, dpi = 300)
-  
-  print(paste("Saved plot:", plot_filename))
+  tryCatch({
+    p <- vis_miss(tempdata_df) + ggtitle(paste("Missing Values in", var))
+    
+    plot_filename <- file.path("05_datavis", paste0(var, "_missing_plot.jpeg"))
+    
+    ggsave(filename = plot_filename, plot = p, width = 8, height = 6, dpi = 300)
+    
+    print(paste("Saved plot:", plot_filename))
+  }, error = function(e) {
+    print(paste("Error in processing", var, ":", e$message))
+  })
 }
