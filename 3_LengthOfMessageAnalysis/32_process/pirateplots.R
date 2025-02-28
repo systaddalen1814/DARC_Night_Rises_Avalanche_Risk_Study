9d01f96713079546b7fa9a66688e0870bf1091d8
nickc0007 <nicholasclausen846@gmail.com>
2025-02-28 11:55:27 -0700

    Yarrrrr

--- 3_LengthOfMessageAnalysis/32_process/pirateplots.R
+++ 3_LengthOfMessageAnalysis/32_process/pirateplots.R
@@ -1,6 +1,19 @@
-library(yarr)
+library(yarrr)
 library(tidyverse)
 
 setwd("~/Desktop/Spring 2025/Capstone/DARC_Night_Rises_Avalanche_Risk_Study/3_LengthOfMessageAnalysis")
 
-dat <- read.csv("/31_input/Master_")
+dat <- read_csv("31_input/Master_Avalanche_Data.csv")
+
+dat <- dat %>% 
+  mutate(Message_Length = nchar(gsub(" ", "", Message)))
+
+pirateplot(
+  data = dat,
+  formula = Message_Length ~ Source,
+  main = "Length of Message by Location",
+  xlab = "AC Location",
+  ylab = "Message Length (Num Characters)"
+)
+
+
